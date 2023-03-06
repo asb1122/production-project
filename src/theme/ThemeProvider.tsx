6274c425dda437f0e835f084ccf48015c798227b
Artsiom Baturyn <artsiombaturyn@MacBook-Pro-Artsiom.local>
2023-03-06 21:09:12 +0400

    initial commit

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,22 @@
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
+import { FunctionComponent, useMemo, useState } from 'react';
+
+const ThemeProvider: FunctionComponent = ({children}) => {
+
+    const dafaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
+
+    const [theme, setTheme] = useState<Theme>(dafaultTheme);
+
+    const defaultProps = useMemo(() => ({
+        theme: theme,
+        setTheme: setTheme
+    }), [theme])
+
+    return (
+        <ThemeContext.Provider value={defaultProps}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export default ThemeProvider;
