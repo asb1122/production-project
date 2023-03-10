895465888defa926012872e91e9e573c76ccff80
Artsiom Baturyn <artsiombaturyn@MacBook-Pro-Artsiom.local>
2023-03-10 22:38:14 +0400

    applinck component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
-import { Link } from 'react-router-dom';
 import "./styles/index.scss";
 import { classNames } from 'shared/lib/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from './providers/router';
+import Navbar from 'widgets/Navbar';
 
 
 const App = () => {
@@ -10,10 +10,9 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>toogle</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>toogle</button>
         </div>
     )
 }
